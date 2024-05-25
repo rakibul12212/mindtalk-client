6c78e2a7bedc487e199db2303a36198680031fc3
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-05-26 01:02:20 +0600

    mission complete

--- src/Pages/Mission/OurMission.jsx
+++ src/Pages/Mission/OurMission.jsx
@@ -3,19 +3,19 @@
 const OurMission = () => {
     return (
         <div className="bg-[#EEF7FF]">
-            <div className=" flex justify-items-center items-center">
-               <div className="container  md:px-20 ps-10 border-2 border-red-200">
-                  <h2 className='text-2xl font-semibold md:font-bold md:text-6xl w-full md:w-3/4  text-[#4D869C]'>What we Do Best</h2>
-                  <h2 className='font-normal w-1/2 md:w-2/4 py-2 md:py-2 text-gray-700'>Our approach to mental health</h2>
-                  <h2 className='font-light w-full md:w-3/4 py-2 md:py-2 text-gray-700'>Use this space to talk about your company's history,background,mission or vissiion.Talk about your phylosophy and how affect your company's trajotory.</h2>
-                  <h2 className='font-light w-full md:w-3/4 py-2 md:py-2 text-gray-700'>Use this space to talk about your company's history,background,mission or vissiion.Talk about your phylosophy and how affect your company's trajotory.</h2>
-               </div>
-               <div className="hidden md:block">
-                 <img src="https://i.ibb.co/j8L3Yv4/ourmission.jpg" alt="img"/>
-               </div>
-            </div>
-
+    <div className="flex justify-between items-center">
+        <div className="container md:px-20 px-10 py-10 md:w-1/2 w-full">
+            <h2 className='text-2xl font-semibold md:font-bold md:text-6xl w-full text-[#4D869C]'>What we Do Best</h2>
+            <h2 className='font-normal w-full py-2 md:py-2 text-gray-700'>Our approach to mental health</h2>
+            <h2 className='font-light w-full py-2 md:py-2 text-gray-700'>Use this space to talk about your company's history, background, mission or vision. Talk about your philosophy and how it affects your company's trajectory.</h2>
+            <h2 className='font-light w-full py-2 md:py-2 text-gray-700'>Use this space to talk about your company's history, background, mission or vision. Talk about your philosophy and how it affects your company's trajectory.</h2>
         </div>
+        <div className="hidden md:block w-1/2">
+            <img src="https://i.ibb.co/j8L3Yv4/ourmission.jpg" alt="img" className="w-full" />
+        </div>
+    </div>
+</div>
+
         
     );
 };
