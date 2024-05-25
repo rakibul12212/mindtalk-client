0b2036022931d2736b3523e9c62b17d86dbc76b1
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-05-25 19:50:11 +0600

    banner

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,7 +2,7 @@
 import Nav from './Nav/Nav';
 import Footer from './Footer/Footer';
 import { Outlet } from 'react-router-dom';
-import Container from '../Component/ui/Container';
+// import Container from '../Component/ui/Container';
 
 const Main = () => {
     return (
@@ -10,9 +10,9 @@ const Main = () => {
         <div>
             
               <Nav />
-              <Container>
+              {/* <Container> */}
               <Outlet/>
-              </Container>
+              {/* </Container> */}
               <Footer/>
             
 
--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,13 +1,51 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './Banner.css';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 
 const Banner = () => {
+    const slides = [
+        {
+             imageUrl: '../../../public/banner.avif', 
+             heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis beatae eaque saepe id distinctio! Nostrum.'
+         },
+        { 
+             imageUrl: '../../../public/banner1.avif',
+             heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis beatae eaque saepe id distinctio! Nostrum.' 
+        },
+        {
+              imageUrl: '../../../public/banner2.avif',
+              heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+              text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis beatae eaque saepe id distinctio! Nostrum.' 
+        },
+        
+    ];
     return (
-        
-        <div>
-            <p>banner</p>
-        </div>
-        
+        <Swiper 
+        autoplay={{ delay: 3000 }}
+        navigation={true}
+        modules={[Navigation, Autoplay]} 
+        className="mySwiper">
+            {slides.map((slide, index) => (
+                <SwiperSlide className='bg-img' key={index} style={{ backgroundImage: `url(${slide.imageUrl})` }}>
+                   
+                   
+                    <div className="slide-content">
+                        <h2 className='p-20 text-6xl w-2/4'>{slide.heading}</h2>
+                        <h2 className='p-20'>{slide.text}</h2>
+                        
+                    </div>
+
+                    
+                </SwiperSlide>
+            ))}
+        </Swiper>
     );
 };
+
 
 export default Banner;
